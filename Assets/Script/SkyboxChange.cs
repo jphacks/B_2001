d8a7b3215b371e39fc5ec93b4e4a700f97041045
kentarou.K <k.takaki_@outlook.com>
2020-11-02 00:13:26 +0900

    Add UI

---
+++ Assets/Script/SkyboxChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChange : MonoBehaviour
+{
+    //事前にInspectorから設定した、Skyboxのマテリアル
+    public Material skybox;
+    public Material skybox2;
+    public Material skybox3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //skyboxの変更
+        RenderSettings.skybox = skybox;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //右クリックを押すとskybox2に入っている背景に切り替わる
+        if (Input.GetMouseButtonDown(1)){
+            RenderSettings.skybox = skybox2;
+        }
+
+        //左クリックを押すとskybox2に入っている背景に切り替わる
+        else if (Input.GetMouseButtonDown(0)) {
+            RenderSettings.skybox = skybox3;
+        }
+
+
+    }
+}
