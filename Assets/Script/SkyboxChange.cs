f1cd4588c30fb82b0c41a70f015495160a9a585c
kenyorha <k.takaki_payday@outlook.com>
2020-11-03 23:04:24 +0900

    Problem fix

--- Assets/Script/SkyboxChange.cs
+++ Assets/Script/SkyboxChange.cs
@@ -10,6 +10,13 @@ public class SkyboxChange : MonoBehaviour
     public Material skybox2;
     public Material skybox3;
     public Material skybox4;
+
+    //判定用変数
+    private bool a = true;
+    private bool b = true;
+    private bool c = true;
+    private bool d = true;
+
     //時間を取得
     private string time;
     // Start is called before the first frame update
@@ -22,31 +29,66 @@ public class SkyboxChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //a,b,c,dがfalseならtrueに変換
+        if (!(a && b && c && d))
+        {
+            a = true;
+            b = true;
+            c = true;
+            d = true;
+        }
+
         time = System.DateTime.Now.Hour.ToString();
         int newtime = int.Parse(time);
 
         //4時から11時まではskybox1の背景
         if (newtime >= 4 && newtime <= 11)
         {
-            RenderSettings.skybox = skybox1;
+            if (a)
+            {
+                RenderSettings.skybox = skybox1;
+                a=false;
+            }
+            else
+            {
+            }
         }
 
-        //12時から18時まではskybox1の背景
+        //12時から15時まではskybox1の背景
         else if (newtime >= 12 && newtime <= 15)
         {
-            RenderSettings.skybox = skybox2;
+            if (b)
+            {
+                RenderSettings.skybox = skybox2;
+                b = false;
+            }
+            else
+            {
+            }
         }
-
         //16時から18時まではskybox1の背景
         else if (newtime >= 16 && newtime <= 18)
         {
-            RenderSettings.skybox = skybox3;
+            if (c)
+            {
+                RenderSettings.skybox = skybox3;
+                c = false;
+            }
+            else
+            {
+            }
         }
         //19時から3時まではskybox1の背景
         else if (newtime >= 19 && newtime <= 24 || newtime >= 0 && newtime <= 3)
         {
-            RenderSettings.skybox = skybox4;
+            if (d)
+            {
+                RenderSettings.skybox = skybox4;
+                d = false;
+            }
+            else
+            { 
+            }
         }
-
     }
 }
