18556555dfdb0025d18ed31061947e42a655637b
K.Arai <k.arai_payday@outlook.com>
2020-11-03 22:56:55 +0900

    add JSON test components:cookie:

---
+++ Assets/Script/ScheduleView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScheduleView : MonoBehaviour
+{
+
+    // スケジュールのオブジェクト
+    [System.Serializable]
+    public class todays_schedule
+    {
+        public int day;
+        public string schedule;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(JsonUtility.ToJson(this));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
