e4ccc0c1b54c2599acf16c39c7815e6608d10ba0
K.Arai <k.arai_payday@outlook.com>
2020-11-04 09:52:25 +0900

    :construction:add new scene and changing scene script

---
+++ Assets/Script/move_Edit_Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class move_Edit_Schedule : MonoBehaviour
+{
+ 
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Edit_Schedule");
+    }
+}
