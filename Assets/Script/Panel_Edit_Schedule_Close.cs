8ccfd37f9aca92b28bb69b1db719f4782e268947
K.Arai <k.arai_payday@outlook.com>
2020-11-04 11:45:32 +0900

    :construction:add Panel_Edit_Schedule

---
+++ Assets/Script/Panel_Edit_Schedule_Close.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Panel_Edit_Schedule_Close : MonoBehaviour, IPointerClickHandler
+{
+
+    [SerializeField]
+    GameObject panel;
+
+    // クリックされたらパネルをvisibleに
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        panel.SetActive(false);
+    }
+}
