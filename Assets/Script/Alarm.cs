6847b7d1333b94ebd1ce64ddaf07f923f5974360
kenyorha <k.takaki_payday@outlook.com>
2020-11-04 15:10:06 +0900

    Help

---
+++ Assets/Script/Alarm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alarm : MonoBehaviour
+{
+
+    //オブジェクトと結びつける
+    public InputField inputField;
+    public Text text;
+    private string newtext;
+    private string time;
+
+    void Start()
+    {
+        //Componentを扱えるようにする
+        inputField = inputField.GetComponent<InputField>();
+        text = text.GetComponent<Text>();
+
+    }
+
+    public void InputText()
+    {
+        newtext = inputField.text;
+        int a = int.Parse(newtext);
+
+        time = System.DateTime.Now.Hour.ToString();
+        int newtime = int.Parse(time);
+
+        //テキストにinputFieldの内容を反映
+        text.text = inputField.text;
+        if (a == newtime)
+        {
+            text.text = "123";
+        }
+
+    }
+
+}
