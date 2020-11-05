2cd763059945e3466a3a1383543b21b31dd64aea
K.Arai <k.arai_payday@outlook.com>
2020-11-05 22:23:01 +0900

    :construction:UI Finished

---
+++ Assets/Script/Time_G.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time_G : MonoBehaviour
+{
+    int m;
+    int t;
+    private Text timetext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = Alarm_H.iptime;
+        m = Alarm_M.iptime_m2;
+        timetext = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timetext.text = t + ":" + m;
+    }
+}
